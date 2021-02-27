e52bf39a12a1a75ea99c9714c4b2b30712fb0840
Avinash Malhotra <avimalhotra505@gmail.com>
2021-02-27 14:13:08 +0530

    Delete src/third-party directory

--- src/third-party/circle.js
+++
@@ -1,9 +0,0 @@
-let r=10;
-const pi=3.14;
-
-var area=pi*r*r;
-
-exports.area=area;
-exports.checkArea=function(){
-    return area;
-}
--- src/third-party/rect.js
+++
@@ -1,3 +0,0 @@
-let l=10, w=6;
-var area=l*w;
-exports.area=area;
